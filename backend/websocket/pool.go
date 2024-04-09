d2da9ff003272f3fcacc16072d9da68d37251bbc
vikassfteng <vikas.sft.engg.1@gmail.com>
2024-04-09 22:48:08 +0530

    Add customwebsocket package with Client and Pool structs

---
+++ backend/websocket/pool.go
@@ -0,0 +1,54 @@
+package customwebsocket
+
+import "fmt"
+
+
+type Pool struct {
+	Register   chan *Client
+	Unregister chan *Client
+	Clients    map[*Client]bool
+	Broadcast  chan Message
+
+}
+
+
+func NewPool() *Pool {
+	return &Pool{
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan Message),
+	}
+}
+
+func (pool *Pool) Start() {
+	for {
+		select {
+		case client := <-pool.Register:
+			pool.Clients[client] = true
+			fmt.Println("total connection pool: ", len(pool.Clients))
+			for k, _ := range pool.Clients {
+				fmt.Println(k)
+				client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
+			}
+			
+		case client := <-pool.Unregister:
+			delete(pool.Clients, client)
+			fmt.Println("total connection pool: ", len(pool.Clients))
+			for k, _ := range pool.Clients {
+				fmt.Println(k)
+				client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."})
+			}
+			
+		case msg := <-pool.Broadcast:
+			fmt.Println("Broadcast message: ")
+
+			for k, _ := range pool.Clients {
+				if err := k.Conn.WriteJSON(msg); err != nil {
+					fmt.Println("error: ", err)
+					return
+				}
+			}
+		}
+	}
+}
